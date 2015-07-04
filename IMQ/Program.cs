2b6ec227bb8c710515dae1b28f769a7a4b37e083
Sam Bloomberg <sam@redxdev.com>
2015-07-03 21:04:11 -0700

    added interactive mode to IMQ

--- IMQ/Program.cs
+++ IMQ/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using ImageQuery.Canvas;
 using ImageQuery.Environment;
 using ImageQuery.Language;
@@ -14,14 +15,15 @@ namespace IMQ
     {
         private static void PrintUsage()
         {
-            Console.WriteLine("IQM [options] <script>");
+            Console.WriteLine("IQM [options] [filename]");
             Console.WriteLine("Options:");
+            Console.WriteLine("  -c  --interactive             Start in interactive console mode. Inputs and outputs will be automatically defined and saved.");
             Console.WriteLine("  -d  --define <name> <value>   Define a parameter for the script.");
             Console.WriteLine("  -h  --help                    Display this help text.");
             Console.WriteLine("  -i  --input  <name> <path>    Define the location for an input.");
             Console.WriteLine("  -o  --output <name> <path>    Define the location for an output.");
             Console.WriteLine("  -p  --parallel                Enable parallel processing/threading (default: no threading).");
-            Console.WriteLine("        <script>                The path to the IQL script to execute.");
+            Console.WriteLine("        [filename]              The path to the IQL script to execute (ignored in interactive mode).");
         }
 
         public static void Main(string[] args)
@@ -36,8 +38,10 @@ namespace IMQ
             settings.AllowParallel = false;
 
             BitmapCanvasLoader canvasLoader = new BitmapCanvasLoader();
+            LinkedList<string> inputNames = new LinkedList<string>();
             Dictionary<string, string> outputPaths = new Dictionary<string, string>();
             Dictionary<string, string> parameters = new Dictionary<string, string>();
+            bool interactive = false;
             string scriptFilename = string.Empty;
 
             int i = 0;
@@ -76,6 +80,7 @@ namespace IMQ
                         string value = args[i];
 
                         canvasLoader.RegisterName(key, value);
+                        inputNames.AddLast(key);
                         break;
                     }
 
@@ -103,7 +108,7 @@ namespace IMQ
                     }
 
                     case "-d":
-                    case "--d":
+                    case "--define":
                     {
                         ++i;
                         if (i >= args.Length)
@@ -125,6 +130,11 @@ namespace IMQ
                         break;
                     }
 
+                    case "-c":
+                    case "--interactive":
+                        interactive = true;
+                        break;
+
                     default:
                         if (!string.IsNullOrEmpty(scriptFilename))
                         {
@@ -139,9 +149,9 @@ namespace IMQ
                 ++i;
             }
 
-            if (string.IsNullOrEmpty(scriptFilename))
+            if (!interactive && string.IsNullOrEmpty(scriptFilename))
             {
-                Console.WriteLine("No input script was specified.");
+                Console.WriteLine("No input script was specified while running in non-interactive mode.");
                 return;
             }
 
@@ -181,6 +191,13 @@ namespace IMQ
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.White;
             }
+            else if (interactive)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("Heads up! You've defined some output locations, but you'll have to create them in IQL to define their size.");
+                Console.WriteLine("For example: output {0}[100,100]", outputPaths.Keys.First());
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
 
             if (settings.AllowParallel)
             {
@@ -191,102 +208,125 @@ namespace IMQ
                 Console.WriteLine("Running in single-threaded mode");
             }
 
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            Console.WriteLine("Loading IQL...");
             root.SetCanvasLoader(canvasLoader);
 
-            IQueryStatement[] statements = null;
-
-            try
+            if (interactive)
             {
-                statements = LanguageUtilities.ParseFile(scriptFilename);
-            }
-            catch (IOException e)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Unable to open script file:");
-                Console.WriteLine(e);
-                Console.BackgroundColor = ConsoleColor.Black;
-                return;
-            }
-            catch (ParseException e)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Unable to parse IQL:");
-                Console.WriteLine(e);
-                Console.BackgroundColor = ConsoleColor.Black;
-                return;
-            }
-
-            double parseTime = stopwatch.Elapsed.TotalSeconds;
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine("Parse completed in {0} seconds", parseTime);
-            Console.ForegroundColor = ConsoleColor.White;
-            stopwatch.Restart();
-
-            try
-            {
-                foreach (var statement in statements)
+                foreach (var name in inputNames)
                 {
-                    statement.Run(root);
+                    try
+                    {
+                        var stm = new DefineInputStatement() { CanvasName = name };
+                        stm.Run(root);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Unable to register input \"{0}\":", name);
+                        Console.WriteLine(e);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        return;
+                    }
                 }
             }
-            catch (Exception e)
+
+            do
             {
-                Console.BackgroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Unable to execute IQL:");
-                Console.WriteLine(e);
-                Console.BackgroundColor = ConsoleColor.Black;
-                return;
-            }
-
-            double queryTime = stopwatch.Elapsed.TotalSeconds;
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine("Query completed in {0} seconds", queryTime);
-            Console.ForegroundColor = ConsoleColor.White;
-            stopwatch.Restart();
-
-            ICanvas[] canvases = root.GetOutputs();
-
-            foreach (var canvas in canvases)
-            {
-                string outputFilename;
-                if (!outputPaths.TryGetValue(canvas.Name, out outputFilename))
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkYellow;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Unknown output {0}, skipping", canvas.Name);
-                    Console.BackgroundColor = ConsoleColor.Black;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    continue;
-                }
-
-                Console.WriteLine("Writing output {0} to {1}...", canvas.Name, outputFilename);
+                IQueryStatement[] statements = null;
 
                 try
                 {
-                    canvas.WriteToFile(outputFilename);
+                    if (!interactive)
+                        statements = LanguageUtilities.ParseFile(scriptFilename);
+                    else
+                    {
+                        Console.Write("IQL> ");
+                        statements = LanguageUtilities.ParseString(Console.ReadLine());
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Unable to load script:");
+                    Console.WriteLine(e);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    continue;
+                }
+                catch (ParseException e)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Unable to parse IQL:");
+                    Console.WriteLine(e);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    continue;
+                }
+
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                try
+                {
+                    foreach (var statement in statements)
+                    {
+                        statement.Run(root);
+                    }
                 }
                 catch (Exception e)
                 {
-                    Console.BackgroundColor = ConsoleColor.DarkYellow;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Unable to write output {0} to file {1}:", canvas.Name, outputFilename);
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Unable to execute IQL:");
                     Console.WriteLine(e);
                     Console.BackgroundColor = ConsoleColor.Black;
-                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
                 }
-            }
 
-            double writeTime = stopwatch.Elapsed.TotalSeconds;
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine("Write completed in {0} seconds", writeTime);
-            Console.ForegroundColor = ConsoleColor.White;
-            stopwatch.Stop();
+                double queryTime = stopwatch.Elapsed.TotalSeconds;
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Query completed in {0} seconds", queryTime);
+                Console.ForegroundColor = ConsoleColor.White;
+                stopwatch.Restart();
 
-            Console.WriteLine("Total time: {0} seconds", parseTime + queryTime + writeTime);
+                ICanvas[] canvases = root.GetOutputs();
+
+                foreach (var canvas in canvases)
+                {
+                    string outputFilename;
+                    if (!outputPaths.TryGetValue(canvas.Name, out outputFilename))
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Unknown output {0}, skipping", canvas.Name);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
+
+                    Console.WriteLine("Writing output {0} to {1}...", canvas.Name, outputFilename);
+
+                    try
+                    {
+                        canvas.WriteToFile(outputFilename);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Unable to write output {0} to file {1}:", canvas.Name, outputFilename);
+                        Console.WriteLine(e);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    }
+                }
+
+                double writeTime = stopwatch.Elapsed.TotalSeconds;
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Write completed in {0} seconds", writeTime);
+                Console.ForegroundColor = ConsoleColor.White;
+                stopwatch.Stop();
+
+                Console.WriteLine("Total time: {0} seconds", queryTime + writeTime);
+            } while (interactive);
         }
     }
 }
